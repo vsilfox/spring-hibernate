5a720a8a7cba37c823133cc86022aa307ea0318f
Vasilisa Nichipor <vsilfox@gmail.com>
2023-02-07 23:42:39 +0300

    simplified DAO

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.TypedQuery;
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -29,23 +28,14 @@ public class UserDaoImp implements UserDao {
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public List<User> getUserByCarModelAndSerial(String model, int series) {
-        List<User> returnList = new ArrayList<>();
 
-        String hql = "From User user left join Car car on user.car = car.user " +
-                     "where car.model = :model and car.series = :series";
+        String hql = "select user from User user left join Car car on user.car = car.user " +
+                     "where car.model = :model AND car.series = :series";
 
-        Query<User> query = sessionFactory.getCurrentSession().createQuery(hql);
+        Query<User> query = sessionFactory.getCurrentSession().createQuery(hql, User.class);
         query.setParameter("model", model);
         query.setParameter("series", series);
-
-        List<User> resultList = query.list();
-
-        for (Object object : resultList) {
-            Object[] row = (Object[]) object;
-            returnList.add((User) row[0]);
-        }
-        return returnList;
+        return query.list();
     }
 }
